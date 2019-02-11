724535c75d557692e6f9e3c406d1b9f2d6b47289
Oscar Salazar <oscarsalazarsevilla@outlook.com>
2019-02-11 16:59:27 -0600

    First Controller - Products

---
+++ Shop/Shop.Web/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shop.Web.Data.Entities;
+
+namespace Shop.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
