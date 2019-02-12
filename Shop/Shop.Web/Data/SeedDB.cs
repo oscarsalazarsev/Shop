45d676db783a4db1072b424b1a2c76d9bdd3a044
Oscar Salazar <oscarsalazarsevilla@outlook.com>
2019-02-11 18:31:57 -0600

    Seed Database

---
+++ Shop/Shop.Web/Data/SeedDB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shop.Web.Data.Entities;
+
+namespace Shop.Web.Data
+{
+    public class SeedDB
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDB(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("iPhone X");
+                this.AddProduct("iPhone 8");
+                this.AddProduct("iPhone SX");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string productName)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = productName,
+                Price = this.random.Next(100),
+                IsAvailable = true,
+                Stock = this.random.Next(100),
+            });
+        }
+    }
+}
--- Shop/Shop.Web/Startup.cs
+++ Shop/Shop.Web/Startup.cs
@@ -32,6 +32,8 @@ namespace Shop.Web
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddTransient<SeedDB>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
